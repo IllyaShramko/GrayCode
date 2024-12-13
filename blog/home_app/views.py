41c83bd65d915ebd2f414bd8aa9ee03e052d6144
Illya Shramko <123illya123123r@gmail.com>
2024-12-13 20:53:42 +0200

    C:/Program Files/Git/

--- blog/home_app/views.py
+++ blog/home_app/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def render_home(request):
-    return render(request, "home_app/home_app.html")
+    return render(request, "home_app/home_app.html", context={"is_auth": False})
