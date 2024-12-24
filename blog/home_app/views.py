492ea62051e5c59b0a7bbd1eff0c404cd8fef5d9
David Petrenko <121516101+Davidptn@users.noreply.github.com>
2024-12-24 21:58:25 +0200

    add contact

--- blog/home_app/views.py
+++ blog/home_app/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def render_home(request):
-    return render(request, "home_app/home_app.html", context= {"is_auth": False})
+    # username = None
+    return render(request, "home_app/home_app.html", context={"is_auth": request.user.is_authenticated, 'username': request.user})
